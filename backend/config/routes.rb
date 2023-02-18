804e66f6581ad868cf38ad35c32db2ca2e9447d4
takahiro_umeda <takahiro.umeda@pixta.co.jp>
2023-02-18 23:13:49 +0900

    graphql_devise導入

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount_graphql_devise_for LoginUser, at: 'graphql_auth'
   if Rails.env.development?
     mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
   end
