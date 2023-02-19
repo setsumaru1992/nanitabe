857c56252cda14266073ca2e9e8b0c97dcd0ddb7
takahiro_umeda <takahiro.umeda@pixta.co.jp>
2023-02-19 11:07:09 +0900

    フロントエンド側でサーバサイドと繋ぐ前準備

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,34 +1,21 @@
-import App from '../components/App'
-import InfoBox from '../components/InfoBox'
-import Header from '../components/Header'
-import Submit from '../components/Submit'
-import PostList, {
-  ALL_POSTS_QUERY,
-  allPostsQueryVars,
-} from '../components/PostList'
 import buildApolloClient, { addApolloState } from '../lib/graphql/buildApolloClient'
 
 const IndexPage = () => (
-  <App>
-    <Header />
-    <InfoBox>ℹ️ This page shows how to use SSG with Apollo.</InfoBox>
-    <Submit />
-    <PostList />
-  </App>
+  <>index</>
 )
 
-export async function getStaticProps() {
-  const apolloClient = buildApolloClient()
-
-  await apolloClient.query({
-    query: ALL_POSTS_QUERY,
-    variables: allPostsQueryVars,
-  })
-
-  return addApolloState(apolloClient, {
-    props: {},
-    revalidate: 1,
-  })
-}
+// export async function getStaticProps() {
+  // const apolloClient = buildApolloClient()
+  //
+  // await apolloClient.query({
+  //   query: ALL_POSTS_QUERY,
+  //   variables: allPostsQueryVars,
+  // })
+  //
+  // return addApolloState(apolloClient, {
+  //   props: {},
+  //   revalidate: 1,
+  // })
+// }
 
 export default IndexPage
