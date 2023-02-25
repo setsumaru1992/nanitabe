f6dd6a1e88020205d5bf8ff8cd6a0662f15fe3ad
takahiro_umeda <takahiro.umeda@pixta.co.jp>
2023-02-25 20:20:00 +0900

    rspec初期化

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -11,7 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_02_23_061612) do
-  create_table "login_users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "login_users", force: :cascade do |t|
     t.string "provider", default: "email", null: false
     t.string "uid", default: "", null: false
     t.string "encrypted_password", default: "", null: false
--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -12,6 +12,8 @@ kill -9 $pre_process
 # RAILS_ENV=${RAILS_ENV} ./bin/rails assets:precompile # M1Macで動かなかったから、graphiql以外のviewをいじるまでコメントアウト
 RAILS_ENV=${RAILS_ENV} bundle exec rails s -p $port -b "0.0.0.0" &
 
+./bin/rails db:migrate RAILS_ENV=test
+
 logfile="log/development.log"
 touch $logfile # 起動時はないため明示的に作成
 tail -f $logfile > /dev/null
