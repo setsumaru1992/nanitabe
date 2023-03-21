3ee26bd2bb3481c4d201fe167ab5d455ff737c5f
takahiro_umeda <takahiro.umeda@pixta.co.jp>
2023-03-21 10:58:58 +0900

    testをitに変更

--- frontend/components/auth/Login.spec.tsx
+++ frontend/components/auth/Login.spec.tsx
@@ -14,36 +14,42 @@ const enterTextBox = (screen, testId, value) => {
 
 const getLoginMessage = (screen) => screen.getByTestId('loginResultMessage');
 
-test('when login succeeds', async () => {
-  const validLoginMock = {
-    request: {
-      query: LoginDocument,
-      variables: {
-        email: 'hogehoge@gmail.com',
-        password: 'hogehoge',
-      },
-    },
-    result: {
-      data: {
-        loginUserLogin: {
-          credentials: {
-            uid: 'hogehoge@gmail.com',
+describe('<Login>', () => {
+  describe('when login with valid params, ', () => {
+    it('succeeds', async () => {
+      const validLoginMock = {
+        request: {
+          query: LoginDocument,
+          variables: {
+            email: 'hogehoge@gmail.com',
+            password: 'hogehoge',
           },
         },
-      },
-    },
-  };
-  render(
-    <MockedProvider mocks={[validLoginMock]}>
-      <Login />
-    </MockedProvider>,
-  );
-  enterTextBox(screen, 'email', 'hogehoge@gmail.com');
-  enterTextBox(screen, 'password', 'hogehoge');
-  fireEvent.click(screen.getByTestId('loginButton'));
+        result: {
+          data: {
+            loginUserLogin: {
+              credentials: {
+                uid: 'hogehoge@gmail.com',
+              },
+            },
+          },
+        },
+      };
+      render(
+        <MockedProvider mocks={[validLoginMock]}>
+          <Login />
+        </MockedProvider>,
+      );
+      enterTextBox(screen, 'email', 'hogehoge@gmail.com');
+      enterTextBox(screen, 'password', 'hogehoge');
+      fireEvent.click(screen.getByTestId('loginButton'));
 
-  await waitFor(() => getLoginMessage(screen));
-  expect(getLoginMessage(screen)).toHaveTextContent('ログインが成功しました');
+      await waitFor(() => getLoginMessage(screen));
+      expect(getLoginMessage(screen)).toHaveTextContent(
+        'ログインが成功しました',
+      );
+    });
+  });
 });
 
 const server = setupServer(
