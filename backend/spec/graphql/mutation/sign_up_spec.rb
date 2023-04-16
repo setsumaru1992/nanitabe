98f27b27e2d85788edcfe0d98edceb4e16657dc7
takahiro_umeda <takahiro.umeda@pixta.co.jp>
2023-04-16 17:53:18 +0900

    失敗テストを修正

--- backend/spec/graphql/mutation/sign_up_spec.rb
+++ backend/spec/graphql/mutation/sign_up_spec.rb
@@ -28,22 +28,24 @@ end
 RSpec.describe Mutations::SignUp, type: :request do
   context "when signup with invalid parameters" do
     it "signup failed" do
+      user_record_size_before = ::User.all.size
       variables = { email: "", password: "" }
       fetch_mutation(build_signup_mutation, variables)
 
       expect(::LoginUser.where(uid: variables[:email]).present?).to eq false
-      expect(::User.all.present?).to eq false
+      expect(::User.all.size).to eq user_record_size_before
     end
   end
 
   context "when signup with valid parameters" do
     it "signup succeed" do
+      user_record_size_before = ::User.all.size
       variables = { email: "hogehoge@gmail.com", password: "hogehogepassword" }
       fetch_mutation(build_signup_mutation, variables)
 
       login_user = LoginUser.where(uid: variables[:email])
       expect(login_user.size).to eq 1
-      expect(login_user.first.user.present?).to eq true
+      expect(::User.all.size).to eq user_record_size_before + 1
     end
   end
 end
