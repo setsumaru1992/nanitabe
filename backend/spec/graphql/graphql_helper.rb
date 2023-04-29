5e938d828fba804d68d1e72678f3e2bcbeb3df6c
takahiro_umeda <takahiro.umeda@pixta.co.jp>
2023-04-29 12:29:16 +0900

    テスト時のgraphqlエラーを吐き出せるよう修正

--- backend/spec/graphql/graphql_helper.rb
+++ backend/spec/graphql/graphql_helper.rb
@@ -3,7 +3,19 @@ def fetch_mutation(mutation_string, variables, headers: {})
   response_body = JSON.parse(response.body)
 
   if response_body["errors"]
-    error_messages = response_body["errors"].map { |error| error["message"] }
+    error_messages = response_body["errors"].map do |error|
+      summary = error["message"]
+      detail = if error["extensions"].blank?
+                 []
+               elsif error["extensions"]["detailed_errors"].present? # graphql-deviseエラー
+                 error["extensions"]["detailed_errors"]
+               elsif error["extensions"]["problems"].present? # 通常のgraphqlエラー
+                 error["extensions"]["problems"].map { |problem| problem["explanation"] }
+               else
+                 []
+               end.compact.join(" and ")
+      "#{summary} #{detail.present? ? "[detail: #{detail}]" : ''}"
+    end
     raise error_messages.join("\n")
   end
   response_body["data"]
