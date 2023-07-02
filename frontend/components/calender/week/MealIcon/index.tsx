818a73473197810075acd890555eec55a91a50ce
takahiro_umeda <takahiro.umeda@pixta.co.jp>
2023-07-02 17:14:54 +0900

    カレンダーの食事にdish.dish_source.typeごとのマーク記載

--- frontend/components/calender/week/MealIcon/index.tsx
+++ frontend/components/calender/week/MealIcon/index.tsx
@@ -5,6 +5,8 @@ import { MealForCalender } from '../../../../lib/graphql/generated/graphql';
 import { MEAL_TYPE } from '../../../../features/meal/const';
 import useFloatModal from '../../../common/modal/useFloatModal';
 import Menu from './Menu';
+import { MEAL_POSITION } from '../../../../features/dish/const';
+import { DISH_SOURCE_TYPE } from '../../../../features/dish/source/const';
 
 type Props = {
   meal: MealForCalender;
@@ -31,8 +33,35 @@ export default (props: Props) => {
     })();
     return `meal-icon--${suffix}`;
   })();
+
+  const mealPositionMark = (() => {
+    const iconCssClass = (() => {
+      if (dish?.dishSourceRelation?.type === DISH_SOURCE_TYPE.RESTAURANT) {
+        return 'fa-solid fa-store';
+      }
+
+      switch (dish.mealPosition) {
+        case MEAL_POSITION.STAPLE_FOOD:
+          return 'fa-solid fa-bowl-rice';
+        case MEAL_POSITION.MAIN_DISH:
+          return 'fa-solid fa-utensils';
+        case MEAL_POSITION.SIDE_DISH:
+          return 'fa-solid fa-carrot';
+        case MEAL_POSITION.SOUP:
+          return 'fa-solid fa-mug-saucer';
+        case MEAL_POSITION.DESSERT:
+          return 'fa-solid fa-cookie-bite';
+        default:
+          return null;
+      }
+    })();
+    if (!iconCssClass) return null;
+    return <i className={iconCssClass} />;
+  })();
+
   return (
     <div className={classnames(style['icon'], style[iconCssClassOfMealType])}>
+      {mealPositionMark && <>{mealPositionMark} &nbsp;</>}
       {dish.name} &nbsp;
       {canAnythingExeptDisplayDishName && (
         <FloatModalOpener>
