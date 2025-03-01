8af25bc5d8ed0e6b08e384fd949226d4aedd9c35
setsumaru1992 <setsumaru1992@gmail.com>
2025-03-01 09:20:25 +0900

    ネイティブアプリについて、バックグラウンドで通信切れたときに復旧できるよう修正

--- frontend/nativeApp/App.tsx
+++ frontend/nativeApp/App.tsx
@@ -1,12 +1,33 @@
-import React from 'react';
-import {SafeAreaView, StyleSheet} from 'react-native';
+import React, {useRef, useEffect} from 'react';
+import {AppState, SafeAreaView, StyleSheet} from 'react-native';
 import {WebView} from 'react-native-webview';
 
 function App(): React.JSX.Element {
+  const appState = useRef(AppState.currentState);
+  const webViewRef = useRef(null);
+
+  useEffect(() => {
+    const subscription = AppState.addEventListener('change', nextAppState => {
+      if (
+        appState.current.match(/inactive|background/) &&
+        nextAppState === 'active'
+      ) {
+        // フォアグラウンドに戻ったときの処理
+        if (webViewRef.current) {
+          webViewRef.current.reload();
+        }
+      }
+      appState.current = nextAppState;
+    });
+
+    return () => subscription.remove();
+  }, []);
+
   return (
     <SafeAreaView style={styles.container}>
       <WebView
         source={{uri: 'https://nanitabe.kibotsu.com/calender/week/thisweek'}}
+        ref={webViewRef}
         style={{flex: 1}}
       />
     </SafeAreaView>
